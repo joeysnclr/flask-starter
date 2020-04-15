afb3555895a552f566ffc684095401c5b299060f
Joseph Sinclair <joey.sinclair02@gmail.com>
2020-04-15 16:09:31 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+import json, os
+
+app = Flask(__name__)
+port = int(os.environ.get('PORT', 5000))
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+
+app.run(host='0.0.0.0', port=port)
