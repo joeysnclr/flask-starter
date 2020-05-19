5192db2a14592e9c42d584623e6a5d95850fbf28
Joseph Sinclair <joey.sinclair02@gmail.com>
2020-05-18 20:37:09 -0700

    added scss, added debug mode, changed css/js tags

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-import json, os
+import json
+import os
 
 app = Flask(__name__)
 port = int(os.environ.get('PORT', 5000))
 
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 
-app.run(host='0.0.0.0', port=port)
+app.run(host='0.0.0.0', port=port, debug=True)
