74e3bc211b84ee4027c2f6de94fc657eb4b2536d
Joey Sinclair <joey.sinclair02@gmail.com>
2020-06-18 02:49:36 +0000

    mongodb and .env

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 import json
 import os
+import dotenv
+import pymongo
+
+# load .env file
+if not os.path.exists('.env'):
+    os.system("touch .env")
+dotenv.load_dotenv()
+
+
+# init MongoDB connection
+
+client = pymongo.MongoClient(os.environ.get("mongodb_url"))
+db = client['db-name']
+collection = db['collection-name']
+
 
 app = Flask(__name__)
 port = int(os.environ.get('PORT', 5000))
